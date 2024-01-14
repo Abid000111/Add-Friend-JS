e20d2755790c1434e30d7e2dfa02512e4e7b907a
Abid Shahrier <abidofficial0001@gmail.com>
2024-01-14 20:59:28 +0600

    Added README.md and formatted all files again properly

---
+++ README.md
@@ -0,0 +1,5 @@
+# Add Friend card
+
+This repository is based on basic HTML, CSS, JS to create a card which has a button to add friend and to remove from friend
+
+Author - Abid Shahrier
--- script.js
+++ script.js
@@ -5,17 +5,17 @@ let h5 = document.querySelector("h5");
 
 let check = true;
 add.addEventListener("click", () => {
-    if(check === true){
-        check = false;
-        h5.innerText = "Friend";
-        h5.style.color = "Green";
-        add.innerHTML = "Remove Friend";
-    }else{
-        check = true;
-        add.innerHTML = "Add Friend";
-        h5.innerHTML = "Stranger";
-        h5.style.color = "red";
-    }
+	if (check === true) {
+		check = false;
+		h5.innerText = "Friend";
+		h5.style.color = "Green";
+		add.innerHTML = "Remove Friend";
+	} else {
+		check = true;
+		add.innerHTML = "Add Friend";
+		h5.innerHTML = "Stranger";
+		h5.style.color = "red";
+	}
 });
 // remove.addEventListener("click", () => {
 //     h5.innerText = "Stranger";
